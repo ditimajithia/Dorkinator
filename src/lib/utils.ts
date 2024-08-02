2acc67152f31939b92119dec38c2b394cc3be2e2
Diti <ditimajthia00@gmail.com>
2024-08-02 12:59:21 +0100

    Connected pocketbase from azure

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,4 +6,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const pocketbase = new PocketBase("http://127.0.0.1:8090");
+export const pocketbase = new PocketBase("http://20.56.210.28:8090/");

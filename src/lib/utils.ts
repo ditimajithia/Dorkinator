f8227c7dc700ec76475c5e666ec17dee8affd354
Diti <ditimajthia00@gmail.com>
2024-08-04 22:19:22 +0100

    changes in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,4 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const pocketbase = new PocketBase("http://20.56.210.28:3000");
+// export const pocketbase = new PocketBase("http://20.56.210.28:3000");
+const POCKETBASE_URL =
+  process.env.REACT_APP_POCKETBASE_URL || "https://dorkinator.pockethost.io";
+export const pocketbase = new PocketBase(POCKETBASE_URL);
